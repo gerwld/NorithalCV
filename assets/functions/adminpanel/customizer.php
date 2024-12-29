ef29a0ce1a18ec01d53ada9a3d8594419fa8ac8c
P Jaworski <pjaworski.dev@gmail.com>
2024-12-29 21:21:54 +0100

    Organized: helpers, customizer set_menus_panel

--- assets/functions/adminpanel/customizer.php
+++ assets/functions/adminpanel/customizer.php
@@ -1,47 +1,34 @@
 <?php
+// Theme customizer.php
 
 
-//Theme customizer.php new settings
+// - Sets the new panel
 function set_menus_panel($wp_customize)
 {
   $wp_customize->add_panel(
     'menu_select_panel',
     array(
       'title' => 'Header settings',
-      'description' => "Most important settings for the elements in the header section.",
+      'description' => "Most essential configurations for the elements in the header section.",
       'priority' => 10,
     )
   );
 
-  //header title settings
+
+
+  // -- Header title section
   $wp_customize->add_section('header_sect_title', array(
     'title' => 'Header Title',
     'priority' => 10,
     'panel' => 'menu_select_panel'
   ));
-
-  $wp_customize->add_section('header_sect_subtitle', array(
-    'title' => 'Header Subtitle',
-    'priority' => 10,
-    'panel' => 'menu_select_panel'
-  ));
-
+  // --- Header title section -> setting 
   $wp_customize->add_setting('header_setting_title', array(
     'validate_callback' => 'true_validate_header',
     'sanitize_callback' => 'true_sanitize_header',
     'default' => 'Arturo Feeney'
   ));
-
-  $wp_customize->add_setting('header_setting_desc', array(
-    'validate_callback' => 'true_validate_header',
-    'sanitize_callback' => 'true_sanitize_header',
-    'default' => 'Interior Designer'
-  ));
-
-  $wp_customize->add_setting('header_subtitle', array(
-    'default' => false
-  ));
-
+  // ---- Header title section -> setting -> control
   $wp_customize->add_control('header_title_control', array(
     'label' => 'Change header title',
     'type' => 'text',
@@ -49,106 +36,30 @@ function set_menus_panel($wp_customize)
     'settings' => 'header_setting_title',
   ));
 
+
+
+  // -- Header subtitle section
+  $wp_customize->add_section('header_sect_subtitle', array(
+    'title' => 'Header Subtitle',
+    'priority' => 10,
+    'panel' => 'menu_select_panel'
+  ));
+  // --- Header subtitle section -> setting 
+  $wp_customize->add_setting('header_setting_desc', array(
+    'validate_callback' => 'true_validate_header',
+    'sanitize_callback' => 'true_sanitize_header',
+    'default' => 'Interior Designer'
+  ));
+  // ---- Header subtitle section -> setting -> control
   $wp_customize->add_control('header_subtitle_control', array(
     'label' => 'Change header subtitle',
     'type' => 'text',
     'section' => 'header_sect_subtitle',
     'settings' => 'header_setting_desc',
   ));
-
-  //  $wp_customize->add_control('header_subtitle_control', array(
-  //   'label' => 'Show header description',
-  //   'description' => "*for change the description go to the site settings tab.",
-  //   'type' => 'checkbox',
-  //   'section' => 'header_sect_title',
-  //   'settings' => 'header_subtitle',
-  //  ));
-  //header title settings end
-
-  //primary section contact button
-  //  $wp_customize->add_section(
-  //   'contactbtn_menu_primary_section',
-  //   array(
-  //    'title' => 'Header Button',
-  //    'priority' => 10,
-  //    'panel' => 'menu_select_panel'
-  //   )
-  //  );
-
-  $wp_customize->add_setting(
-    'contactbtn_menu_primary_text',
-    array(
-      'validate_callback' => 'true_validate_cbtn_text',
-      'sanitize_callback' => 'sanitize_text_field',
-      'default' => 'Contact Us'
-    )
-  );
-  $wp_customize->add_setting(
-    'contactbtn_menu_primary_link',
-    array(
-      'validate_callback' => 'true_validate_cbtn_link',
-      'sanitize_callback' => 'sanitize_text_field',
-      'default' => '#contact-us'
-    )
-  );
-  $wp_customize->add_setting(
-    'contactbtn_menu_primary_show',
-    array(
-      'default' => true
-    )
-  );
-  $wp_customize->add_setting(
-    'contactbtn_menu_primary_innewwindow',
-    array(
-      'default' => false
-    )
-  );
-
-  $wp_customize->add_control(
-    'contactbtn_menu_primary_control-3',
-    array(
-      'label' => 'Show button',
-      'type' => 'checkbox',
-      'section' => 'contactbtn_menu_primary_section',
-      'settings' => 'contactbtn_menu_primary_show',
-    )
-  );
-
-  $wp_customize->add_control(
-    'contactbtn_menu_primary_control-1',
-    array(
-      'label' => 'Button text',
-      'type' => 'text',
-      'section' => 'contactbtn_menu_primary_section',
-      'settings' => 'contactbtn_menu_primary_text',
-    )
-  );
-
-  $wp_customize->add_control(
-    'contactbtn_menu_primary_control-2',
-    array(
-      'label' => 'Button link',
-      'type' => 'text',
-      'description' => 'http://*, https://*, /* or #id',
-      'section' => 'contactbtn_menu_primary_section',
-      'settings' => 'contactbtn_menu_primary_link',
-    )
-  );
-
-  $wp_customize->add_control(
-    'contactbtn_menu_primary_control-4',
-    array(
-      'label' => 'Open link in a new tab',
-      'type' => 'checkbox',
-      'section' => 'contactbtn_menu_primary_section',
-      'settings' => 'contactbtn_menu_primary_innewwindow',
-    )
-  );
-  //primary section contact button end
 }
+
 add_action('customize_register', 'set_menus_panel');
-
-
 
 
 
--- assets/functions/helpers.php
+++ assets/functions/helpers.php
@@ -1,18 +1,22 @@
 <?php
 
-// Add console.log alternative
-function consolelog($data)
-{
-  $output = $data;
-  if (is_array($output)) $output = implode(',', $output);
-  echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-}
+ /** Adds console.log alternative to PHP (front-end only).
+ * @param any $data Any data to display in console.log */
+
+    function consolelog($data)
+    {
+      $output = $data;
+      if (is_array($output)) $output = implode(',', $output);
+      echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+    }
 
 
+ /** Injects SVG file as a string `<svg .... />`.
+ * @param string $relative_path Relative svg path to base_dir 
+ * @return string **SVG content** / **\<!-- SVG not found --\>** html comment line */
 
-// Injects inline svg's if they exist
-function inline_svg($relative_path)
-{
-  $svg_file = get_template_directory() . '/' . ltrim($relative_path, '/');
-  return (file_exists($svg_file)) ? file_get_contents($svg_file) : '<!-- SVG not found -->';
-}
+    function inline_svg($relative_path)
+    {
+      $svg_file = get_template_directory() . '/' . ltrim($relative_path, '/');
+      return (file_exists($svg_file)) ? file_get_contents($svg_file) : '<!-- SVG not found -->';
+    }

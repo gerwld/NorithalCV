ed3736380ff6a6025606a4ce3a08d1b6ae3af56c
P Jaworski <pjaworski.dev@gmail.com>
2025-01-05 23:43:06 +0100

    mv projects to /assets/pages

---
+++ assets/pages/projects.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Projects Page (norithal-cv)
+ */
+
+ 
+ get_template_part('assets/sections/ld-block.php'); 
+ get_template_part('assets/sections/ld-block.php'); 
+ get_template_part('assets/sections/ld-block.php'); 
+ get_template_part('assets/sections/ld-block.php'); 
+ 
+ 
+ ?>
